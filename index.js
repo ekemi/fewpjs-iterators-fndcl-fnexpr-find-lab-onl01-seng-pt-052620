0f7083d9aa20658e74566f1bbc3497245bbb6e85
edmundo papi <edmundopierre@yahoo.com>
2020-12-18 13:20:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  return record.find(function(re){
+    if (re.result==="W"){
+      return (re).year;
+    }
+    else {
+      return undefined;
+    }
+  })
+}
